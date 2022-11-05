4db7daef9497b2de43b5a387e0ba0d9d385643e7
Salehkhatib <skhatib552@gmail.com>
2022-11-05 11:51:06 +0100

    Password-Toggle

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+
+let x = document.getElementById("myInput");
+if (x.type === "password") {
+    x.type = "text";
+    document.getElementById("hide").style.display = "inline-block";
+    document.getElementById("show").style.display = "none";
+}
+else{
+    x.type = "password";
+    document.getElementById("hide").style.display = "none";
+    document.getElementById("show").style.display = "inline-block";
+}
+}
